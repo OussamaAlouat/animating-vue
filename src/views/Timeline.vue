5a277a2188296fba1e99b4b658fed091bc99ffc9
oussamaAlouat <al317257@uji.es>
2019-12-18 15:46:21 +0100

    Added timeline animations

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -11,7 +11,10 @@ import gsap from 'gsap'
 
 export default {
   mounted() {
-    // timeline will go here
+    let tl = gsap.timeline({ repeat: 2 })
+    tl.to('.first', { x: 700, duration: 2, ease: 'expo.out' })
+    tl.to('.second', { x: 700, duration: 2, ease: 'expo.out' })
+    tl.to('.third', { x: 700, duration: 2, ease: 'expo.out' })
   }
 }
 </script>
