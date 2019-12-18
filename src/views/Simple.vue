6eeb7bff007eddaf82bee25b4042ccbd8bd4bfc7
oussamaAlouat <al317257@uji.es>
2019-12-18 12:48:11 +0100

    Added simple component

---
+++ src/views/Simple.vue
@@ -0,0 +1,30 @@
+<template>
+  <transition appear @before-enter="beforeEnter" @enter="enter" :css="false">
+    <div class="card"></div>
+  </transition>
+</template>
+
+<script>
+export default {
+  methods: {
+    beforeEnter(el) {
+      // starting style
+    },
+    enter(el, done) {
+      // style to transition to once entered
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  display: block;
+  margin: 0 auto 0 auto;
+  height: 6.5em;
+  width: 6.5em;
+  border-radius: 1%;
+  background-color: #16c0b0;
+  box-shadow: 0.08em 0.03em 0.4em #ababab;
+}
+</style>
