314a336db2fd57149490c3d5951e942725dfe77a
oussamaAlouat <al317257@uji.es>
2019-12-18 12:25:58 +0100

    Used drawe component

--- src/router.js
+++ src/router.js
@@ -29,6 +29,12 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "list" */ './views/List.vue')
+    },
+    {
+      path: '/drawer',
+      name: 'drawer',
+      component: () =>
+        import(/* webpackChunkName: "list" */ './views/Drawer.vue')
     }
   ]
 })
