c60cc044f49e1b61dc465e4e85fea4b92006960a
oussamaAlouat <al317257@uji.es>
2019-12-18 11:40:23 +0100

    Added list component

---
+++ src/views/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input v-model="newContact" placeholder="Name" type="text" />
+    <button @click="addContact">Add Contact</button>
+    <ul>
+      <li v-for="(contact, index) in contacts" :key="index">
+        {{ contact }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newContact: '',
+      contacts: ['Beau Thabeast', 'Cindy Rella', 'Alice Wunderlind']
+    }
+  },
+  methods: {
+    addContact() {
+      this.contacts.push(this.newContact)
+      this.newContact = ''
+    }
+  }
+}
+</script>
