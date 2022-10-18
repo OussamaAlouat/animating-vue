91726be95196293c72493e26be66bd165324cbb5
OussamaAlouat <al317257@uji.es>
2022-10-18 12:58:01 +0200

    Change names to fix lint error and use standard names

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/HomeView.vue'
 
 Vue.use(Router)
 
@@ -20,7 +20,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ './views/About.vue')
+        import(/* webpackChunkName: "about" */ './views/AboutView.vue')
     },
     {
       path: '/list',
@@ -28,43 +28,43 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "list" */ './views/List.vue')
+      component: () => import(/* webpackChunkName: "list" */ './views/ListView.vue')
     },
     {
       path: '/drawer',
       name: 'drawer',
       component: () =>
-        import(/* webpackChunkName: "list" */ './views/Drawer.vue')
+        import(/* webpackChunkName: "list" */ './views/DrawerView.vue')
     },
     {
       path: '/cards',
       name: 'cards',
       component: () =>
-        import(/* webpackChunkName: "list" */ './views/Cards.vue')
+        import(/* webpackChunkName: "list" */ './views/CardsView.vue')
     },
     {
       path: '/simple',
       name: 'simple',
       component: () =>
-        import(/* webpackChunkName: "simple" */ './views/Simple.vue')
+        import(/* webpackChunkName: "simple" */ './views/SimpleView.vue')
     },
     {
       path: '/stagger',
       name: 'stagger',
       component: () =>
-        import(/* webpackChunkName: "stagger" */ './views/Stagger.vue')
+        import(/* webpackChunkName: "stagger" */ './views/StaggerView.vue')
     },
     {
       path: '/state',
       name: 'state',
       component: () =>
-        import(/* webpackChunkName: "state" */ './views/State.vue')
+        import(/* webpackChunkName: "state" */ './views/StateView.vue')
     },
     {
       path: '/timeline',
       name: 'timeline',
       component: () =>
-        import(/* webpackChunkName: "timeline" */ './views/Timeline.vue')
+        import(/* webpackChunkName: "timeline" */ './views/TimelineView.vue')
     }
   ]
 })
